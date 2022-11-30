f49361dc0321c9e8a384ca5acfe9abb0b3cb697e
Yaqoob <sualehfarooq65@gmail.com>
2022-11-30 01:18:03 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import { BsFillPencilFill } from "react-icons/bs";
 import { GiNotebook } from "react-icons/gi";
 import {TbClipboardText} from 'react-icons/tb'
 import Button from "react-bootstrap/Button";
-import { addTodo } from "./containers/addData.js";
+import { addTodo } from "./containers/addData";
 
 function App() {
+  function checkVal(event) {
+    console.log(event)
+  }
   var data;
   function check(event) {
     console.log(event.target.value)
@@ -18,9 +21,23 @@ function App() {
    console.log(inpData)
    if(inpData.value.trim()==="") {
     alert('Add Value')
-   } 
-   
+   }  else {
+   document.getElementById('addTodo').innerHTML += 
+   `
+   <li> ${inpData.value} <button id='editBtn'>Edit</button> </li>
+   `
   }
+}
+function detAll() {
+  document.getElementById('addTodo').innerHTML = ""
+}
+
+let editBtn = document.getElementById('editBtn')
+
+if(editBtn) {
+  console.log('Button Clicked')
+}
+
   return (
     <div className="space-con">
       <h2>
@@ -32,14 +49,13 @@ function App() {
           <div className="input">
             <input onChange={check} id='inpSpace' className="input-space" type="text" placeholder="Enter Your Todo Here" />
             <Button onClick={add}> Add Todo</Button>
-            <Button> Delete All</Button>
+            <Button onClick={detAll}> Delete All</Button>
           </div>
           <div className="listSpace"> 
           <ul id="addTodo" >
           </ul>
           </div>
         </div>
-      <addTodo datach ='Sualeh' />
       </div>
     </div>
   );
